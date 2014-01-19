35c0207480b6158e22ed39993fbc5fb620d919e9
Zorro <zorro@appaddict.org>
2014-01-19 02:53:07 +0100

    Initial Commit

---
+++ yopainstaller/main.m
@@ -0,0 +1,21 @@
+//
+//  main.m
+//  yopainstaller
+//
+//  Created by Zorro on 19.01.14.
+//  Copyright (c) 2014 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main (int argc, const char * argv[])
+{
+
+    @autoreleasepool
+    {	
+    	// insert code here...
+    	NSLog(@"Hello, World!");	
+    }
+	return 0;
+}
+
